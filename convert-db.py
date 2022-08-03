a05434071caa1154b3c313df612f7b39bb78aa0a
Zoia Butenko <55627796+zojabutenko@users.noreply.github.com>
2022-08-03 18:06:11 +0200

    Update convert-db.py

--- convert-db.py
+++ convert-db.py
@@ -44,61 +44,62 @@ def write_record(row, record):
     with open(record_to_filename(record), "w") as f:
         f.write("---\n")
         f.write(f"record: {record}\n")
-        f.write(f"name: '{row['Name'].strip()}'\n")
-        # f.write(f"UD_name: '{row['Name UD'].strip()}'\n")
-        f.write(f"formula_gloss: '{row['Gloss for the formula'].strip()}'\n")
+        f.write(f"name: \"{row['Name'].strip()}\"\n")
+        f.write(f"formula_gloss_russian: \"{row['Gloss for the formula (Russian)'].strip()}\"\n")
+        f.write(f"formula_gloss_english: \"{row['Gloss for the formula (English)'].strip()}\"\n")
+
 
         f.write(f"illustration: \"{row['Illustration'].strip()}\"\n")
-        f.write(f"illustration_gloss_russian: '{row['Gloss for the illustration (Russian)'].strip()}'\n")
-        f.write(f"illustration_gloss_english: '{row['Gloss for the illustration (English)'].strip()}'\n")
-        f.write(f"illustration_translation_russian: '{row['Translation for the illustration (Russian)'].strip()}'\n")
-        f.write(f"illustration_translation_english: '{row['Translation for the illustration (English)'].strip()}'\n")
+        f.write(f"illustration_gloss_russian: \"{row['Gloss for the illustration (Russian)'].strip()}\"\n")
+        f.write(f"illustration_gloss_english: \"{row['Gloss for the illustration (English)'].strip()}\"\n")
+        f.write(f"illustration_translation_russian: \"{row['Translation for the illustration (Russian)'].strip()}\"\n")
+        f.write(f"illustration_translation_english: \"{row['Translation for the illustration (English)'].strip()}\"\n")
 
         f.write("definitions:\n")
         for language in ["Russian", "English"]:
             entry = row[f"Definition in {language}"].strip()
             if entry != "":
                 f.write(f"  - {language.lower()}: |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
         f.write("examples:\n")
         for column in ["Example 1: sentence", "Example 2: sentence", "Example 3: sentence"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
         f.write("examples_glosses_russian:\n")
         for column in ["Example 1: gloss (Russian)", "Example 2: gloss (Russian)", "Example 3: gloss (Russian)"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
 
         f.write("examples_glosses_english:\n")
         for column in ["Example 1: gloss (English)", "Example 2: gloss (English)", "Example 3: gloss (English)"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
 
         f.write("examples_translation_russian:\n")
         for column in ["Example 1: translation (Russian)", "Example 2: translation (Russian)", "Example 3: translation (Russian)"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
 
         f.write("examples_translation_english:\n")
         for column in ["Example 1: translation (English)", "Example 2: translation (English)", "Example 3: translation (English)"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
         f.write("examples_sources:\n")
         for column in ["Example 1: source", "Example 2: source", "Example 3: source"]:
             entry = row[column].strip()
             if entry != "":
                 f.write("  - |\n")
-                f.write(f"       {entry}\n")
+                f.write(f"       \"{entry}\"\n")
 
         f.write("morphology:\n")
         f.write(print_as_yaml_list(row["Morphology"], split_sequence))
@@ -114,7 +115,7 @@ def write_record(row, record):
         entry = row["Comment"].strip()
         if entry != "":
             f.write("comment: |\n")
-            f.write(f"    '{entry}'\n")
+            f.write(f"    \"{entry}\"\n")
 
         f.write("references:\n")
         f.write("  - |\n")
